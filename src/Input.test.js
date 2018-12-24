37c24799e0fbb824c1db5ebebab954b3af8ed3b7
Denis Denes <denes.dennis@gmail.com>
2018-12-24 13:11:38 +0200

    Clear input box on submit and test

--- src/Input.test.js
+++ src/Input.test.js
@@ -110,5 +110,9 @@ describe('`guessWord` action creator call', () => {
   test('calls `guessWord` with input value as argument', () => {
     const guessWordArg = guessWordMock.mock.calls[0][0];
     expect(guessWordArg).toBe(guessedWord);
-  })
+  });
+
+  test('input box clears on submit', () => {
+    expect(wrapper.instance().inputBox.current.value).toBe('');
+  });
 });
