9aee4b119c1be8c2df7c93940c747eda7e59a9c5
Denis Denes <denes.dennis@gmail.com>
2018-12-24 12:32:39 +0200

    Connect App component to redux and add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React       from 'react';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { storeFactory } from "../test/testUtils";
+import App              from './App';
+
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  return shallow(<App store={store} />).dive();
+};
+
+describe('redux properties', () => {
+  test('has access to `success` state', () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+
+  test('has access to `secretWord` state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+
+  test('has access to `guessedtWords` state', () => {
+    const guessedWords = [{ guessedtWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+
+  test('`getSecretWord` action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  })
 });
