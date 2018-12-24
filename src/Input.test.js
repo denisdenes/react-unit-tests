8f831482a240cc42068a15931bfda7e9f1db4b64
Denis Denes <denes.dennis@gmail.com>
2018-12-24 02:02:55 +0200

    Use Enzyme .dive to see inside the component

--- src/Input.test.js
+++ src/Input.test.js
@@ -6,11 +6,9 @@ import Input                            from './Input';
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<Input store={store} />).dive();
+  // console.log(wrapper.debug());
 };
-
-setup();
 
 describe('render', () => {
   describe('word has not been guessed', () => {
